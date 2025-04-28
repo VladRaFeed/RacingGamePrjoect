e136af0e646b157ba5094d71726d1d42a3df3cc1
VladRaFeed <122728801+VladRaFeed@users.noreply.github.com>
2025-04-28 17:25:42 +0300

    add connect to db

--- src/score/score.py
+++ src/score/score.py
@@ -1,3 +1,26 @@
+import os
+from dotenv import load_dotenv
+from pymongo import MongoClient
+
+load_dotenv()
+
+MONGO_URL = os.getenv("MONGO_URL")
+
+client = MongoClient(MONGO_URL)
+
+db = client.racingDb
+collection = db.score
+
+# collections = db.list_collection_names()
+# for name in collections:
+#     print(f" - {name}")
+# collection = db.score
+
+elements = collection.find()
+
+for element in elements:
+    print(element)
+
 class Scoreboard:
     def __init__(self):
         super().__init__()
